a91761c604c67fc44af5ec731bd49696210732f9
Dan Hill <danh32@gmail.com>
2022-12-02 11:24:21 -0600

    prep for day 3, fix readme links

---
+++ src/main/kotlin/co/danhill/aoc/year2022/Day03.kt
@@ -0,0 +1,22 @@
+package co.danhill.aoc.year2022
+
+import co.danhill.aoc.util.Day
+import co.danhill.aoc.util.Input
+import co.danhill.aoc.util.lines
+
+fun main() = Day03.run("2022/03.txt")
+
+object Day03 : Day<List<String>>() {
+
+    override fun parseInput(input: Input): List<String> {
+        return input.lines
+    }
+
+    override fun part1(input: List<String>): Any {
+        TODO()
+    }
+
+    override fun part2(input: List<String>): Any {
+        TODO()
+    }
+}
