b6aa1612ceb736fa38d264c8beb30399563ef8d8
Dan Hill <danh32@gmail.com>
2022-12-04 09:52:48 -0600

    day 4 solution

--- src/main/kotlin/co/danhill/aoc/year2022/Day04.kt
+++ src/main/kotlin/co/danhill/aoc/year2022/Day04.kt
@@ -4,19 +4,41 @@ import co.danhill.aoc.util.Day
 import co.danhill.aoc.util.Input
 import co.danhill.aoc.util.lines
 
-fun main() = Day03.run("2022/04.txt")
+fun main() = Day04.run("2022/04.txt")
 
-object Day04 : Day<List<String>>() {
+private typealias Assignments = Pair<IntRange, IntRange>
 
-    override fun parseInput(input: Input): List<String> {
-        return input.lines
+object Day04 : Day<List<Assignments>>() {
+
+    override fun parseInput(input: Input): List<Assignments> {
+        return input.lines.map { line ->
+            val (e1, e2) = line.split(',').map { range ->
+                val (start, end) = range.split('-')
+                    .map { it.toInt() }
+                start..end
+            }
+            e1 to e2
+        }
     }
 
-    override fun part1(input: List<String>): Any {
-        TODO()
+    override fun part1(input: List<Assignments>): Any {
+        return input.count { (a, b) ->
+            a.contains(b) || b.contains(a)
+        }
     }
 
-    override fun part2(input: List<String>): Any {
-        TODO()
+    override fun part2(input: List<Assignments>): Any {
+        return input.count { (a, b) -> a.overlaps(b) }
+    }
+
+    private fun IntRange.contains(other: IntRange): Boolean {
+        return this.contains(other.first) && this.contains(other.last)
+    }
+
+    private fun IntRange.overlaps(other: IntRange): Boolean {
+        return this.contains(other.first)
+                || this.contains(other.last)
+                || other.contains(this.first)
+                || other.contains(this.last)
     }
 }
