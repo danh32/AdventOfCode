afdd27b2808d6e796333f94f4cfb11f915b302e5
Dan Hill <danh32@gmail.com>
2022-12-06 11:09:30 -0600

    day 6 solution

--- src/main/kotlin/co/danhill/aoc/year2022/Day06.kt
+++ src/main/kotlin/co/danhill/aoc/year2022/Day06.kt
@@ -2,20 +2,36 @@ package co.danhill.aoc.year2022
 
 import co.danhill.aoc.util.Day
 import co.danhill.aoc.util.Input
-import co.danhill.aoc.util.lines
+import co.danhill.aoc.util.text
 
-fun main() = Day06.run("2022/06_test.txt")
+fun main() = Day06.run("2022/06.txt")
 
-object Day06 : Day<List<String>>() {
-    override fun parseInput(input: Input): List<String> {
-        return input.lines
+object Day06 : Day<String>() {
+    override fun parseInput(input: Input): String {
+        return input.text
     }
 
-    override fun part1(input: List<String>): Any {
-        TODO()
+    override fun part1(input: String): Any {
+        return input.startOfPacketMarkerIndex
     }
 
-    override fun part2(input: List<String>): Any {
-        TODO()
+    override fun part2(input: String): Any {
+        return input.startOfMessageMarkerIndex
+    }
+
+    private val String.startOfPacketMarkerIndex: Int
+        get() = findRunOfUniqueChars(4)
+
+    private val String.startOfMessageMarkerIndex: Int
+        get() = findRunOfUniqueChars(14)
+
+    private fun String.findRunOfUniqueChars(count: Int): Int {
+        return withIndex()
+            .windowed(count)
+            .first { indexedChars ->
+                indexedChars.map { it.value }.toSet().size == count
+            }
+            .last()
+            .index + 1
     }
 }
