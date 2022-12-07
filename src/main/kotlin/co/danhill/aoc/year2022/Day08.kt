1c7af5aa9256fe9b5aad476a691fa78f5028b269
Dan Hill <danh32@gmail.com>
2022-12-07 14:41:22 -0600

    prepping for day 8

---
+++ src/main/kotlin/co/danhill/aoc/year2022/Day08.kt
@@ -0,0 +1,21 @@
+package co.danhill.aoc.year2022
+
+import co.danhill.aoc.util.Day
+import co.danhill.aoc.util.Input
+import co.danhill.aoc.util.lines
+
+fun main() = Day08.run("2022/08_test.txt")
+
+object Day08 : Day<List<String>>() {
+    override fun parseInput(input: Input): List<String> {
+        return input.lines
+    }
+
+    override fun part1(input: List<String>): Any {
+        TODO()
+    }
+
+    override fun part2(input: List<String>): Any {
+        TODO()
+    }
+}
