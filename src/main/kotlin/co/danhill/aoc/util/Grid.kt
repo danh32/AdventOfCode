c3c0b71c1908943d2e742870a0bddab9f3d538bd
Dan Hill <danh32@gmail.com>
2022-12-08 11:46:24 -0600

    day 8 solution

--- src/main/kotlin/co/danhill/aoc/util/Grid.kt
+++ src/main/kotlin/co/danhill/aoc/util/Grid.kt
@@ -8,11 +8,42 @@ interface Grid<T> : MutableMap<Point, T> {
     val maxX: Int
     val minY: Int
     val maxY: Int
+
     fun print(
         xRange: IntRange = minX..maxX,
         yRange: IntRange = minY..maxY,
         toString: (T?) -> String = { it.toString() },
     )
+
+    fun Grid<Int>.sequenceToLeftEdge(start: Point): Sequence<Int> = sequenceBetween(start, 0 to start.y)
+    fun Grid<Int>.sequenceToTopEdge(start: Point): Sequence<Int> = sequenceBetween(start, start.x to 0)
+    fun Grid<Int>.sequenceToRightEdge(start: Point): Sequence<Int> = sequenceBetween(start, maxX to start.y)
+    fun Grid<Int>.sequenceToBottomEdge(start: Point): Sequence<Int> = sequenceBetween(start, start.x to maxY)
+
+    fun Grid<Int>.sequenceBetween(start: Point, end: Point): Sequence<Int> {
+        val (startX, startY) = start
+        val (endX, endY) = end
+        if (startX == endX && startY == endY)
+            return sequenceOf(this.getValue(start))
+
+        return sequence {
+            when {
+                startX == endX -> {
+                    val range = if (startY < endY) startY..endY else startY.downTo(endY)
+                    for (y in range) {
+                        yield(this@sequenceBetween.getValue(startX to y))
+                    }
+                }
+                startY == endY -> {
+                    val range = if (startX < endX) startX..endX else startX.downTo(endX)
+                    for (x in range) {
+                        yield(this@sequenceBetween.getValue(x to startY))
+                    }
+                }
+                else -> error("$start and $end must share a row or column")
+            }
+        }
+    }
 }
 
 private class MapGrid<T>(
@@ -64,6 +95,7 @@ private class MapGrid<T>(
 }
 
 fun <T> gridOf(): Grid<T> = MapGrid()
+fun <T> gridOf(data: MutableMap<Point, T>): Grid<T> = MapGrid(data)
 
 fun Collection<String>.toIntGrid(): Grid<Int> {
     val grid = gridOf<Int>()
--- src/main/kotlin/co/danhill/aoc/year2022/Day08.kt
+++ src/main/kotlin/co/danhill/aoc/year2022/Day08.kt
@@ -1,21 +1,49 @@
 package co.danhill.aoc.year2022
 
-import co.danhill.aoc.util.Day
-import co.danhill.aoc.util.Input
-import co.danhill.aoc.util.lines
+import co.danhill.aoc.util.*
 
-fun main() = Day08.run("2022/08_test.txt")
+fun main() = Day08.run("2022/08.txt")
 
-object Day08 : Day<List<String>>() {
-    override fun parseInput(input: Input): List<String> {
+object Day08 : Day<Grid<Int>>() {
+    override fun parseInput(input: Input): Grid<Int> {
         return input.lines
+            .toIntGrid()
     }
 
-    override fun part1(input: List<String>): Any {
-        TODO()
+    override fun part1(input: Grid<Int>): Any {
+        return input.keys.count { input.isTreeVisibleFromOutside(it) }
     }
 
-    override fun part2(input: List<String>): Any {
-        TODO()
+    override fun part2(input: Grid<Int>): Any {
+        return input.keys.maxOf { point ->
+            input.scenicScore(point)
+        }
+    }
+
+    private fun Grid<Int>.isTreeVisibleFromOutside(point: Point): Boolean {
+        val height = this.getValue(point)
+        fun Sequence<Int>.isVisibleFromOutside(): Boolean = drop(1).all { it < height }
+
+        if (sequenceToLeftEdge(point).isVisibleFromOutside()) return true
+        if (sequenceToTopEdge(point).isVisibleFromOutside()) return true
+        if (sequenceToRightEdge(point).isVisibleFromOutside()) return true
+        return sequenceToBottomEdge(point).isVisibleFromOutside()
+    }
+
+    private fun Grid<Int>.scenicScore(point: Point): Int {
+        val height = getValue(point)
+        fun Sequence<Int>.viewingDistance(): Int {
+            var count = 0
+            for (currentHeight in this.drop(1)) {
+                count++
+                if (currentHeight >= height) return count
+            }
+            return count
+        }
+        val leftViewingDistance = sequenceToLeftEdge(point).viewingDistance()
+        val topViewingDistance = sequenceToTopEdge(point).viewingDistance()
+        val rightViewingDistance = sequenceToRightEdge(point).viewingDistance()
+        val bottomViewingDistance = sequenceToBottomEdge(point).viewingDistance()
+        return leftViewingDistance * topViewingDistance * rightViewingDistance * bottomViewingDistance
     }
 }
