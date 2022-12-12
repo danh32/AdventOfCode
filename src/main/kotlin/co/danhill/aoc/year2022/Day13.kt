b2e3bcfd8308ae0c208e64828573659b6b1eeda5
Dan Hill <danh32@gmail.com>
2022-12-12 12:18:21 -0600

    prepping for day 13

---
+++ src/main/kotlin/co/danhill/aoc/year2022/Day13.kt
@@ -0,0 +1,20 @@
+package co.danhill.aoc.year2022
+
+import co.danhill.aoc.util.Day
+import co.danhill.aoc.util.Input
+import co.danhill.aoc.util.lines
+
+fun main() = Day13.run("2022/13_test.txt")
+
+object Day13 : Day {
+
+    private fun Input.parse() = lines
+
+    override fun part1(input: Input): Any {
+        TODO()
+    }
+
+    override fun part2(input: Input): Any {
+        TODO()
+    }
+}
