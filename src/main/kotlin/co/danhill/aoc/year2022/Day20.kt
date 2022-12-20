6c466cd94090a770de15f52fae19754224b715c5
Dan Hill <danh32@gmail.com>
2022-12-20 13:10:14 -0600

    cleanup

--- src/main/kotlin/co/danhill/aoc/year2022/Day20.kt
+++ src/main/kotlin/co/danhill/aoc/year2022/Day20.kt
@@ -8,45 +8,39 @@ fun main() = Day20.run("2022/20.txt")
 
 object Day20 : Day {
 
-    private fun Input.parse() = lines.map { it.toInt() }
+    private fun Input.parse() = lines.map { it.toLong() }
 
     override fun part1(input: Input): Any {
-        val original = input.parse()
-        val mixed = original.withIndex().toMutableList()
-
-        original.forEachIndexed { originalIndex, number ->
-            val index = mixed.indexOfFirst { (i, n) -> n == number && i == originalIndex }
-            val value = mixed.removeAt(index)
-            val newIndex = (((index + number) % mixed.size) + mixed.size) % mixed.size
-            mixed.add(newIndex, value)
-        }
-
-        val indexOfZero = mixed.indexOfFirst { (_, n) -> n == 0 }
-        val a = mixed[(indexOfZero + 1000) % mixed.size].value
-        val b = mixed[(indexOfZero + 2000) % mixed.size].value
-        val c = mixed[(indexOfZero + 3000) % mixed.size].value
-        println("$a + $b + $c")
-        return a + b + c
+        return input.parse()
+            .mix()
+            .groveCoordinateSum()
     }
 
     override fun part2(input: Input): Any {
-        val original = input.parse().map { it * 811589153L }
-        val mixed = original.withIndex().toMutableList()
-        println(original)
-        repeat(10) {
-            original.forEachIndexed { originalIndex, number ->
+        return input.parse()
+            .map { it * 811589153L }
+            .mix(10)
+            .groveCoordinateSum()
+    }
+
+    private fun List<Long>.mix(times: Int = 1): List<Long> {
+        val mixed = this.withIndex().toMutableList()
+        repeat(times) {
+            this.forEachIndexed { originalIndex, number ->
                 val index = mixed.indexOfFirst { (i, n) -> n == number && i == originalIndex }
                 val value = mixed.removeAt(index)
                 val newIndex = (((index + number) % mixed.size) + mixed.size) % mixed.size
                 mixed.add(newIndex.toInt(), value)
             }
         }
+        return mixed.map { it.value }
+    }
 
-        val indexOfZero = mixed.indexOfFirst { (_, n) -> n == 0L }
-        val a = mixed[(indexOfZero + 1000) % mixed.size].value
-        val b = mixed[(indexOfZero + 2000) % mixed.size].value
-        val c = mixed[(indexOfZero + 3000) % mixed.size].value
-        println("$a + $b + $c")
+    private fun List<Long>.groveCoordinateSum(): Long {
+        val indexOfZero = this.indexOfFirst { it == 0L }
+        val a = this[(indexOfZero + 1000) % this.size]
+        val b = this[(indexOfZero + 2000) % this.size]
+        val c = this[(indexOfZero + 3000) % this.size]
         return a + b + c
     }
 }
