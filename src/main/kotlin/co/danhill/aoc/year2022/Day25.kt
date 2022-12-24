f0d3a7dd2d380825cf1627e2a281e69df278b5ed
Dan Hill <danh32@gmail.com>
2022-12-24 15:55:16 -0600

    prepping for day 25

---
+++ src/main/kotlin/co/danhill/aoc/year2022/Day25.kt
@@ -0,0 +1,20 @@
+package co.danhill.aoc.year2022
+
+import co.danhill.aoc.util.Day
+import co.danhill.aoc.util.Input
+import co.danhill.aoc.util.lines
+
+fun main() = Day25.run("2022/25_test.txt")
+
+object Day25 : Day {
+
+    private fun Input.parse() = this.lines
+
+    override fun part1(input: Input): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: Input): Any {
+        TODO("Not yet implemented")
+    }
+}
