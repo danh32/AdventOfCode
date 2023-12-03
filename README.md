fc1a1c97798a43d366236293cb27cf2f44e3ba29
Dan Hill <danh32@gmail.com>
2023-12-03 11:00:37 -0600

    2023 Day 02

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a simple repo for me to do the annual [Advent of Code](https://adventofc
 
 ## 2023
 
-2022 sources are [here](/src/main/kotlin/co/danhill/aoc/year2023).
+2023 sources are [here](/src/main/kotlin/co/danhill/aoc/year2023).
 
 ## 2022
 
---
+++ src/main/kotlin/co/danhill/aoc/year2023/Day02.kt
@@ -0,0 +1,58 @@
+package co.danhill.aoc.year2023
+
+import co.danhill.aoc.util.Day
+import co.danhill.aoc.util.Input
+import co.danhill.aoc.util.lines
+import kotlin.math.max
+
+fun main() = Day02.run("2023/02.txt")
+
+object Day02 : Day {
+    override fun part1(input: Input): Any {
+        val limits = mapOf(
+            "red" to 12,
+            "green" to 13,
+            "blue" to 14,
+        )
+        return input.lines.sumOf { line ->
+            val (game, rounds) = line.split(": ")
+            val gameId = game.split(" ")[1]
+            val map = rounds.split("; ")
+                .fold(mutableMapOf<String, Int>()) { map, round ->
+                    val draws = round.split(", ")
+                    for (draw in draws) {
+                        val (amount, color) = draw.split(" ")
+                        map.compute(color) { _, amt ->
+                            max(amt ?: 0, amount.toInt())
+                        }
+                    }
+                    map
+                }
+
+            val passes = map.entries.all { (color, amount) ->
+                amount <= limits.getValue(color)
+            }
+            if (passes) gameId.toInt() else 0
+        }
+    }
+
+    override fun part2(input: Input): Any {
+        return input.lines.sumOf { line ->
+            val (_, rounds) = line.split(": ")
+            val map = rounds.split("; ")
+                .fold(mutableMapOf<String, Int>()) { map, round ->
+                    val draws = round.split(", ")
+                    for (draw in draws) {
+                        val (amount, color) = draw.split(" ")
+                        map.compute(color) { _, amt ->
+                            max(amt ?: 0, amount.toInt())
+                        }
+                    }
+                    map
+                }
+
+            val power = map.values.fold(1) { power, value -> power * value }
+            power
+        }
+    }
+}
